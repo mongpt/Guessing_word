a862f877ee06db1ec1336d5965f1cc86c8fea378
mongpt <mongphantan@gmail.com>
2022-11-08 20:11:22 +0200

    done

---
+++ Guessing_Word.py
@@ -0,0 +1,18 @@
+import random
+
+wordList = ["apple", "banana", "castle", "president", "engineer", "university", "school", "capital"]
+randomWord = random.choice(wordList)
+resultList = []
+for i in randomWord:
+    resultList.append("_")
+print("A new word =",resultList)
+while "_" in resultList:
+    guess = input("Guess a letter of this word: ")
+    if guess in randomWord:
+        for i in range(len(randomWord)):
+            if guess == randomWord[i]:
+                resultList.insert(i,guess)
+                resultList.pop(i+1)
+    print("A new word =",resultList)
+print(">>>>>>>",randomWord,"<<<<<<<")
+
